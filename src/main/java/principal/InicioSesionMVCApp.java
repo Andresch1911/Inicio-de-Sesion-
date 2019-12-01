e3ace7af27ba93a65c3c5ca5ef0d7a5bcba98353
Andresch1911 <55486755+Andresch1911@users.noreply.github.com>
2019-12-01 18:43:25 +0100

    Firts Commit

---
+++ src/main/java/principal/InicioSesionMVCApp.java
@@ -0,0 +1,36 @@
+package principal;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class InicioSesionMVCApp extends Application {
+
+	private Controller controller = new Controller();
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		controller.getView().getCancelButton().setOnAction(e -> onCancelButtonAction(e));
+
+		Scene scene = new Scene(controller.getView(), 340, 180);
+
+		primaryStage.setTitle("Iniciar Sesión");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	private void onCancelButtonAction(ActionEvent e) {
+
+		Stage stage = (Stage) controller.getView().getCancelButton().getScene().getWindow();
+		stage.close();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ src/main/java/principal/Main.java
@@ -0,0 +1,15 @@
+package principal;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("" + "[i] Usuarios y contraseñas:\n\n" + "[>] chuck:norris\n" + "[>] charles:bronson\n"
+				+ "[>] david:carradine\n" + "[>] test:test\n");
+
+		InicioSesionMVCApp.main(args);
+	}
+
+}
